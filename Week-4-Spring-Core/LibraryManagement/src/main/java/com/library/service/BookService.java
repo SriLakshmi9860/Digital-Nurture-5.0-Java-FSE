5764dd2d5fa4158920c837a538527f3c7606a367
Vundela Srilakshmi <srilakshmi5037@gmail.com>
2026-07-20 21:17:17 +0530

    Completed Week 4 Spring Core Exercises

---
+++ Week-4-Spring-Core/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,28 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Constructor Injection
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // Setter Injection
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void display() {
+        System.out.println("Book Service Bean Created");
+        bookRepository.display();
+    }
+}
